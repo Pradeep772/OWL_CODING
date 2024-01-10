a0f36f270be83cd3d48cfd5880462597b2d91b48
G  G S Pradeep Reddy <97424653+Pradeep772@users.noreply.github.com>
2024-01-10 18:17:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Modular Exponentiation for large numbers/modular-exponentiation-for-large-numbers.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+	def PowMod(self, x, n, m):
+		res=1
+		while(n):
+		    if(n%2):
+		        n-=1
+		        res=((res%m)*(x%m))%m
+		    else:
+		        n=n/2
+		        x=((x%m)*(x%m))%m
+        return res%m
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		x, n , m = input().split()
+		x = int(x)
+		n = int(n) 
+		m = int(m)
+		ob = Solution();
+		ans = ob.PowMod(x, n, m)
+		print(ans)
+# } Driver Code Ends
