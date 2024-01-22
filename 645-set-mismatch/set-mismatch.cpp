474a7f24e4a261f9701e9a0b1a238b92e6466a1e
G  G S Pradeep Reddy <97424653+Pradeep772@users.noreply.github.com>
2024-01-22 18:30:59 +0530

    Time: 675 ms (5.05%) | Memory: 35.5 MB (8.31%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int a,b,c=1;
         set<int>s(nums.begin(),nums.end());
         for(auto it:s){
