5bf06f1ddc57ad0427f8a84aa4f45b0878fe5fc8
G  G S Pradeep Reddy <97424653+Pradeep772@users.noreply.github.com>
2024-01-24 18:57:34 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Buy Stock with Transaction Fee/buy-stock-with-transaction-fee.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long maximumProfit(vector<long long>&arr, int n, int f) {
+        // Code here
+        long long int s=0;
+        long long int b = -arr[0];
+        for(int i=1; i<n; i++){
+            s = max(s, (b+arr[i]-f));
+            b = max(b, s- arr[i]);
+        }
+        return s;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<long long> prices;
+        
+        for(int i=0; i<n; ++i){
+            long long x; cin>>x;
+            prices.push_back(x);
+        }
+        
+        int fee; cin>>fee;
+        
+        Solution obj;
+        cout<<obj.maximumProfit(prices, n, fee)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
