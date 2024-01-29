516ed5887c110d759af89ebb0b85c944c13a0a72
G  G S Pradeep Reddy <97424653+Pradeep772@users.noreply.github.com>
2024-01-29 22:53:08 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Josephus problem/josephus-problem.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+/*You are required to complete this method */
+
+class Solution
+{
+    public:
+    int drunk(int n,int K){
+       if(n==1)return 0;
+       return ((drunk(n-1,K)+K)%n); 
+    }
+    int josephus(int n, int k)
+    {
+       return drunk(n,k)+1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;//taking input n and k
+		
+		//calling josephus() function
+		Solution ob;
+		cout<<ob.josephus(n,k)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
