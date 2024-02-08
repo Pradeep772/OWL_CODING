68da9da7e279afc4e99baa5d7eea6c09c207027e
G  G S Pradeep Reddy <97424653+Pradeep772@users.noreply.github.com>
2024-02-08 18:00:02 +0530

    Time: 67 ms (78.32%) | Memory: 15.3 MB (13.66%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int dp[10001];
-    int drunk(int n){
-        if(n<0)return 1e9;
-        if(n==0)return 0;
-        if(dp[n]!=0)return dp[n];
-        int cnt=0;
-        int ans=INT_MAX;
-        for(int i=1;i<sqrt(n)+1;i++){
-            ans=min(ans,1+drunk(n-(i*i)));
-            
-        }
-        return dp[n]=ans;
-    }
     int numSquares(int n) {
-        // memset(dp,-1,sizeof(dp));
-        return drunk(n);
+        queue<pair<int,int>>q;
+        vector<bool>v(10001);
+        q.push({n,0});
+        int c;
+        while(!q.empty()){
+            int a=q.front().first;
+            int b=q.front().second;
+            q.pop();
+            if(a == 0)return b;
+            for(int i=1;i*i<=a;i++){
+                if(!v[a-(i*i)]){
+                    v[a-(i*i)]=1;
+                    q.push({a-(i*i),b+1});
+                }
+            }
+        }
+        return -1;
     }
 };
