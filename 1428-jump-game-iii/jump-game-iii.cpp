2d8287c85e55923b5f0d1cb3e2dacae21f95e53f
G  G S Pradeep Reddy <97424653+Pradeep772@users.noreply.github.com>
2024-02-22 23:59:36 +0530

    Time: 429 ms (5.15%) | Memory: 49.7 MB (10.84%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
+    
+    int dp[50001];
     bool drunk(vector<int>&arr ,int ind,vector<bool>& vis){
         if(ind<0 || ind>=arr.size())return 0;
         if(arr[ind]==0)return 1;
+        if(dp[ind]!=-1)return dp[ind]; 
         if(vis[ind]==1)return 0;
         vis[ind]=1;
         bool left=drunk(arr,ind-arr[ind],vis);
         bool right=drunk(arr,ind+arr[ind],vis);
         vis[ind]=0;
-        return left||right;
+        return dp[ind]=left||right;
     }
     bool canReach(vector<int>& arr, int start) {
+        memset(dp,-1,sizeof(dp));
         vector<bool>vis(arr.size()-1);
         return drunk(arr,start,vis);
     }
