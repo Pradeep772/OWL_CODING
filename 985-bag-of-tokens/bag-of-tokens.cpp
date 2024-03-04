72eed155ab3f5c255532f84d596e2384632aa20d
G  G S Pradeep Reddy <97424653+Pradeep772@users.noreply.github.com>
2024-03-04 17:35:23 +0530

    Time: 15 ms (18.89%) | Memory: 13.2 MB (23.53%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& t, int p) {
+        sort(t.begin(),t.end());
+        int c=0;
+        int i=0,j=t.size()-1;
+        while(i<=j){
+            if(t[i]>p && c==0){
+                return 0;
+                break;
+            }
+            if(t[i]>p && c!=0){
+                p+=t[j];
+                j--;
+                c--;
+            }
+            if(t[i]<=p){
+                c++;
+                p-=t[i];
+                i++;
+            }
+            cout<<p<<" ";
+        }
+        return c;
+    }
+};
