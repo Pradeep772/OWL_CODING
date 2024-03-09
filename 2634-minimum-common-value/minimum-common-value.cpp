2bf095d899fd6fb636ed79563d9c1532aa1b1cc7
G  G S Pradeep Reddy <97424653+Pradeep772@users.noreply.github.com>
2024-03-09 10:17:07 +0530

    Time: 213 ms (5.09%) | Memory: 84.9 MB (5.15%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        set<int>s1(nums1.begin(),nums1.end());
-        set<int>s2(nums2.begin(),nums2.end());
         map<int,int>mp;
-        for(auto it:s1){
-            mp[it]++;
+        int pre1=0,pre2=0;
+        for(auto it:nums1){
+            if(pre1!=it){
+                mp[it]++;
+                pre1=it;
+            }
+            
         }
-        for(auto it:s2){
-            mp[it]++;
+        for(auto it:nums2){
+            if(pre2!=it){
+                mp[it]++;
+                pre2=it;
+            }
         }
         for(auto it:mp){
             if(it.second>1){
