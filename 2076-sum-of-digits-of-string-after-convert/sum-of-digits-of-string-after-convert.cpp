d65866cb7a2fbb8a4fb002326bbb58d93ab47d75
G  G S Pradeep Reddy <97424653+Pradeep772@users.noreply.github.com>
2024-09-03 10:10:25 +0530

    Time: 7 ms (14.68%) | Memory: 10.1 MB (38.94%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string sum="";
+        for(auto it:s){
+            int n=(it-'a')+1;
+            sum+=to_string(n);
+        }
+        int ans=0;
+        while(k--){
+            for(auto it:sum){
+                ans+=int(it)-48;
+            }
+            if(k!=0){
+                sum=to_string(ans);
+                ans=0;
+            }
+        }
+
+        
+        return ans;
+    }
+};
