8d630b917b1ad7818706da9a5e8672a2a70d9b60
G  G S Pradeep Reddy <97424653+Pradeep772@users.noreply.github.com>
2025-01-17 12:34:02 +0530

    Time: 0 ms (100.00%) | Memory: 264 MB (28.04%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& d) {
+        int a=d[0];
+        for(int i=1;i<d.size();i++){
+            a^=d[i];
+        }
+        return !a;
+        
+    }
+};
