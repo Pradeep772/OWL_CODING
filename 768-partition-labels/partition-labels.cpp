e4125166f83712d8ec490d3bfa5e21bd32129711
G  G S Pradeep Reddy <97424653+Pradeep772@users.noreply.github.com>
2025-03-31 22:57:29 +0530

    Time: 3 ms (30.87%) | Memory: 8.9 MB (49.46%) - LeetSync

---
+++ 768-partition-labels/partition-labels.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int>ans;
+        map<char,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]=i;
+        }
+        int i=0,j=0;
+        while(i<s.size()){
+            int m=mp[s[i]];
+            while(j<=m){
+                m=max(m,mp[s[j]]);
+                j++;
+            }
+            ans.push_back(m-i+1);
+            i=j;
+        }
+
+        return ans;
+    }
+};
